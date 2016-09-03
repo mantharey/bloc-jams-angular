ee8c34a07c0edae54fcc7b3205fd74356734affb
mantharey <Samantha@Macintosh.local>
2016-09-03 19:39:53 -0400

    Assignment 7: Completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
-        
+/**
+* @desc Current song
+* @type {Object}
+*/        
         var currentSong = null;
 /**
 * @desc Buzz object audio file
@@ -27,19 +30,33 @@
             
             currentSong = song;
         };
-        
+/**
+ * @function playSong
+ * @desc Plays currentBuzzObject
+ * @param {Object} song
+ */        
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+/**
+ * @desc Method to play and stop songs
+ * @param method
+ */           
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
-        
+/**
+ * @desc Method to pause songs
+ * @param method
+ */         
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
