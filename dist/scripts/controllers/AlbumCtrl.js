57b487ec8dc1dcbfb80799a70b4ab4d406395fd9
mantharey <Samantha@Macintosh.local>
2016-09-03 19:13:58 -0400

    Checkpoint 7: Completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,10 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
-        this.album = Fixtures.getAlbum();    
+    function AlbumCtrl(Fixtures, SongPlayer) {
+        this.album = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
     }
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
