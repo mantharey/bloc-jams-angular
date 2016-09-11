41a5c706d04d9c8e6a5dff80ca99403f96eae4d3
mantharey <Samantha@Macintosh.local>
2016-09-10 23:11:08 -0400

    Assignment 11: Attempt 1

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,7 +1,7 @@
 (function() {
     function timecode() {
         return function(seconds) {
-            var seconds = Number.parseFloat(seconds);
+            /*var seconds = Number.parseFloat(seconds);
             
             if (Number.isNaN(seconds)) {
                 return '-:--';
@@ -17,8 +17,9 @@
                  output += '0';   
             }
  
-            output += remainingSeconds;
- 
+            output += remainingSeconds;*/
+            var timer = buzz.toTimer(this.getTime());
+            
             return output;
         };
     }
@@ -27,3 +28,6 @@
         .module('blocJams')
         .filter('timecode', timecode);
 })();
+
+
+/*Optional: The Buzz library has a method named toTimer() that formats seconds in an easy to read timer like "00:00", which is what we've just accomplished with the timecode filter. Read the documentation for the toTimer() method and rework the timecode filter logic to use the Buzz method instead of your custom code.*/
