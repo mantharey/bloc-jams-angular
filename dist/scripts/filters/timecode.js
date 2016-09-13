f1b00a65a2ab58eefc185c515533ce9734240755
mantharey <Samantha@Macintosh-3.local>
2016-09-12 21:55:28 -0400

    Assignment 11 - Completed

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -1,26 +1,10 @@
 (function() {
     function timecode() {
         return function(seconds) {
-            /*var seconds = Number.parseFloat(seconds);
+       
+            var timer = buzz.toTimer(seconds);
             
-            if (Number.isNaN(seconds)) {
-                return '-:--';
-            }
-            
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
- 
-            var output = minutes + ':';
- 
-            if (remainingSeconds < 10) {
-                 output += '0';   
-            }
- 
-            output += remainingSeconds;*/
-            var timer = buzz.toTimer(this.getTime());
-            
-            return output;
+            return timer;
         };
     }
 
